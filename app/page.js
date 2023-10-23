ed8bd25921ffa26e10b96163f5ce3807c2d047cc
leuzaoo <leuzaoocontato@gmail.com>
2023-10-23 17:47:48 -0300

    Created components and updated pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
+"use client";
+
+import Hero from "./components/Hero";
+import Loader from "./components/Loader";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div>homepage</div>
-  )
+    <div className={styles.test}>
+      <Hero />
+      <Loader />
+    </div>
+  );
 }
