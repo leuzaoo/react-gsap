29d6673e75d6405cfb8f9caed3daaf44e965fde8
leuzaoo <leuzaoocontato@gmail.com>
2023-10-24 15:44:14 -0300

    Updated

--- app/page.js
+++ app/page.js
@@ -1,14 +1,74 @@
-"use client";
-
-import Hero from "./components/Hero";
-import Loader from "./components/Loader";
-import styles from "./page.module.css";
+import "./page.css";
 
 export default function Home() {
   return (
-    <div className={styles.test}>
-      <Hero />
-      <Loader />
+    <div className="container">
+      <div className="area-model">
+        <h1>ITEM 1</h1>
+      </div>
+      <div className="area-model">
+        <h1>ITEM 2</h1>
+      </div>
+
+      <section className="items">
+        <div className="items-content">
+          <img
+            className="relogio"
+            src="https://lh3.googleusercontent.com/fife/AK0iWDyRKd4Je5-QAhAntv5Se51SVTERtCKTe_HTjCsWFpAOPcoZzmwGk89JJeSA3lKX5l989ZzSDgOCJ44ZLKoNB8YtFXlVMBslVw0Ex1YNWTHOilEWVkE7uTYkJkQRXdz1Af4jDYY5bXC-d8_pZKUkp5U4V7JPpW-348wtK42tQm4mX-KiLE8tUKtlexpDrNuNt0zre9s5UVD1wlAtieMgtdcDmASebya1jP7z0QLePq1kioAvuZkpwbEhI1_3wcpTPmDliYJjJaWpisQWgFcXgfjb5Br2jltz_Z6Af4tkg0yRzQe6dWGoEg-QRmdADSRpsChovXfk_jDami0-lTLD-BXcqWSj9slnbBPXRNOxSh6m3O6UIvOvSiFRaU-wDIjkRVI23gZ2EwP9hxYkqQdvLXZ4-IHwBNek_U9jXG9WmnZE-28JqBrK2k89zwbY3A1Xa6KiqfylgJiCBh2ngaQaLYNbCHcOorVNTofHvBAk1Ad3eBvlxrpxguscDttSy31AyTMabscHTfhljSoi2BE6Nr0biTrRqCaG1jYI08t5aPc3Y0K6u-Umsalw_9hN5lm6XV5pkhMO_eXV-0zA2rWnkOjieOiQSBoz2FA4rVx_qNbnGLDQ9AkjA_NMiHTl4LahcGvloiK36F4Zcoy9oKgH22Pi_Aq75cQoaTPEsteA_1u_iNVcKMz40wME6qlrepV_iAY1SNCmG08SkP044U3vCYUPqZp9IqgDOhdtacWe5AjTn-M4e0jEBVG85bM4TbAAYEFQFgeDrIP0GfKnxYcypmVzipsaqXXzMw4YT7W9h0wZKNzXJ2szV47qj5n67LrtyKbQUcSKuXQylbH8iWZ6kR3ShX6sI2wvS0Jfcash-yBXLwxq-mL1cRL3EbERGl-0tVB-_qDnnc2VOvurbbo8WiM7mZYkbxM9UGyJbMciW6NdYbYu0Bg-bvRRDWu2B4Qwr1GQuQJCrWaSpKGpmfRRs9gwvqft9nGAIFQLgjYjtvUWSFxGJ9JT8qnk3H27qzma-vFTrJCGSo1Y5G-AuGPmZYIhApk4mAPHAn8u5otXn-ayWAM45F4P5vUVn7hAA4epQ1Z7Q6g-voWzJFomAN1wefmMXbWajlJRkhiuq8VdgpvScVLpbyMCB5ZyBHqeu0NSH-FGekNHKJYLVovAXaiMrrlt3YOA2UZ7YjpL4Mvx9cISh0jPZOV7t9yQT6JjrBj672q-tt5nNDWJsXHtj9S8_8rdURlJTTV2fmwycTGSmOdgBYcoZOMQtO-gVwnlYEUOVNQm1QV55A9ZRCG-L7X_MHvVhjtGHyFmK073iMGCgsxVuacGwWPoyeyjh2JTCXFdeZWmMA7SqOQGrhBP-Pv_2uqg-N24H0zxBHC28wLYtFNcN8iF9EDJTnePJBmyXxBGodBj-oC5dUQAJwj1is_KqoKL2ozeQ_meEdl9zU3udKoklx4e9Dcjqt2MW1lDxtt6uW4aHmmgs3rBfF61VAJNVC-Si6K17wsan9kWfD-zJArNx9kSchtddW78K-3tchYoOqjgMhI8hSw=w2880-h1508"
+            alt="Relogio AppleWatch"
+          />
+        </div>
+      </section>
+
+      <section className="models-container">
+        <h2 className="title">Qual é o Apple Watch ideal para você?</h2>
+
+        <div className="models-content">
+          <div className="models-item" id="model-1">
+            <img
+              src="https://lh3.googleusercontent.com/fife/AK0iWDyRKd4Je5-QAhAntv5Se51SVTERtCKTe_HTjCsWFpAOPcoZzmwGk89JJeSA3lKX5l989ZzSDgOCJ44ZLKoNB8YtFXlVMBslVw0Ex1YNWTHOilEWVkE7uTYkJkQRXdz1Af4jDYY5bXC-d8_pZKUkp5U4V7JPpW-348wtK42tQm4mX-KiLE8tUKtlexpDrNuNt0zre9s5UVD1wlAtieMgtdcDmASebya1jP7z0QLePq1kioAvuZkpwbEhI1_3wcpTPmDliYJjJaWpisQWgFcXgfjb5Br2jltz_Z6Af4tkg0yRzQe6dWGoEg-QRmdADSRpsChovXfk_jDami0-lTLD-BXcqWSj9slnbBPXRNOxSh6m3O6UIvOvSiFRaU-wDIjkRVI23gZ2EwP9hxYkqQdvLXZ4-IHwBNek_U9jXG9WmnZE-28JqBrK2k89zwbY3A1Xa6KiqfylgJiCBh2ngaQaLYNbCHcOorVNTofHvBAk1Ad3eBvlxrpxguscDttSy31AyTMabscHTfhljSoi2BE6Nr0biTrRqCaG1jYI08t5aPc3Y0K6u-Umsalw_9hN5lm6XV5pkhMO_eXV-0zA2rWnkOjieOiQSBoz2FA4rVx_qNbnGLDQ9AkjA_NMiHTl4LahcGvloiK36F4Zcoy9oKgH22Pi_Aq75cQoaTPEsteA_1u_iNVcKMz40wME6qlrepV_iAY1SNCmG08SkP044U3vCYUPqZp9IqgDOhdtacWe5AjTn-M4e0jEBVG85bM4TbAAYEFQFgeDrIP0GfKnxYcypmVzipsaqXXzMw4YT7W9h0wZKNzXJ2szV47qj5n67LrtyKbQUcSKuXQylbH8iWZ6kR3ShX6sI2wvS0Jfcash-yBXLwxq-mL1cRL3EbERGl-0tVB-_qDnnc2VOvurbbo8WiM7mZYkbxM9UGyJbMciW6NdYbYu0Bg-bvRRDWu2B4Qwr1GQuQJCrWaSpKGpmfRRs9gwvqft9nGAIFQLgjYjtvUWSFxGJ9JT8qnk3H27qzma-vFTrJCGSo1Y5G-AuGPmZYIhApk4mAPHAn8u5otXn-ayWAM45F4P5vUVn7hAA4epQ1Z7Q6g-voWzJFomAN1wefmMXbWajlJRkhiuq8VdgpvScVLpbyMCB5ZyBHqeu0NSH-FGekNHKJYLVovAXaiMrrlt3YOA2UZ7YjpL4Mvx9cISh0jPZOV7t9yQT6JjrBj672q-tt5nNDWJsXHtj9S8_8rdURlJTTV2fmwycTGSmOdgBYcoZOMQtO-gVwnlYEUOVNQm1QV55A9ZRCG-L7X_MHvVhjtGHyFmK073iMGCgsxVuacGwWPoyeyjh2JTCXFdeZWmMA7SqOQGrhBP-Pv_2uqg-N24H0zxBHC28wLYtFNcN8iF9EDJTnePJBmyXxBGodBj-oC5dUQAJwj1is_KqoKL2ozeQ_meEdl9zU3udKoklx4e9Dcjqt2MW1lDxtt6uW4aHmmgs3rBfF61VAJNVC-Si6K17wsan9kWfD-zJArNx9kSchtddW78K-3tchYoOqjgMhI8hSw=w2880-h1508"
+              alt="Relogio Preto"
+            />
+            <span className="models-tag">Novo</span>
+            <h4 className="models-title">Apple Watch Series 8</h4>
+            <p className="models-description">
+              A partir de <strong>R$ 5.299</strong>
+            </p>
+          </div>
+
+          <div className="models-item" id="model-2">
+            <img
+              src="https://lh3.googleusercontent.com/fife/AK0iWDyRKd4Je5-QAhAntv5Se51SVTERtCKTe_HTjCsWFpAOPcoZzmwGk89JJeSA3lKX5l989ZzSDgOCJ44ZLKoNB8YtFXlVMBslVw0Ex1YNWTHOilEWVkE7uTYkJkQRXdz1Af4jDYY5bXC-d8_pZKUkp5U4V7JPpW-348wtK42tQm4mX-KiLE8tUKtlexpDrNuNt0zre9s5UVD1wlAtieMgtdcDmASebya1jP7z0QLePq1kioAvuZkpwbEhI1_3wcpTPmDliYJjJaWpisQWgFcXgfjb5Br2jltz_Z6Af4tkg0yRzQe6dWGoEg-QRmdADSRpsChovXfk_jDami0-lTLD-BXcqWSj9slnbBPXRNOxSh6m3O6UIvOvSiFRaU-wDIjkRVI23gZ2EwP9hxYkqQdvLXZ4-IHwBNek_U9jXG9WmnZE-28JqBrK2k89zwbY3A1Xa6KiqfylgJiCBh2ngaQaLYNbCHcOorVNTofHvBAk1Ad3eBvlxrpxguscDttSy31AyTMabscHTfhljSoi2BE6Nr0biTrRqCaG1jYI08t5aPc3Y0K6u-Umsalw_9hN5lm6XV5pkhMO_eXV-0zA2rWnkOjieOiQSBoz2FA4rVx_qNbnGLDQ9AkjA_NMiHTl4LahcGvloiK36F4Zcoy9oKgH22Pi_Aq75cQoaTPEsteA_1u_iNVcKMz40wME6qlrepV_iAY1SNCmG08SkP044U3vCYUPqZp9IqgDOhdtacWe5AjTn-M4e0jEBVG85bM4TbAAYEFQFgeDrIP0GfKnxYcypmVzipsaqXXzMw4YT7W9h0wZKNzXJ2szV47qj5n67LrtyKbQUcSKuXQylbH8iWZ6kR3ShX6sI2wvS0Jfcash-yBXLwxq-mL1cRL3EbERGl-0tVB-_qDnnc2VOvurbbo8WiM7mZYkbxM9UGyJbMciW6NdYbYu0Bg-bvRRDWu2B4Qwr1GQuQJCrWaSpKGpmfRRs9gwvqft9nGAIFQLgjYjtvUWSFxGJ9JT8qnk3H27qzma-vFTrJCGSo1Y5G-AuGPmZYIhApk4mAPHAn8u5otXn-ayWAM45F4P5vUVn7hAA4epQ1Z7Q6g-voWzJFomAN1wefmMXbWajlJRkhiuq8VdgpvScVLpbyMCB5ZyBHqeu0NSH-FGekNHKJYLVovAXaiMrrlt3YOA2UZ7YjpL4Mvx9cISh0jPZOV7t9yQT6JjrBj672q-tt5nNDWJsXHtj9S8_8rdURlJTTV2fmwycTGSmOdgBYcoZOMQtO-gVwnlYEUOVNQm1QV55A9ZRCG-L7X_MHvVhjtGHyFmK073iMGCgsxVuacGwWPoyeyjh2JTCXFdeZWmMA7SqOQGrhBP-Pv_2uqg-N24H0zxBHC28wLYtFNcN8iF9EDJTnePJBmyXxBGodBj-oC5dUQAJwj1is_KqoKL2ozeQ_meEdl9zU3udKoklx4e9Dcjqt2MW1lDxtt6uW4aHmmgs3rBfF61VAJNVC-Si6K17wsan9kWfD-zJArNx9kSchtddW78K-3tchYoOqjgMhI8hSw=w2880-h1508"
+              alt="Relogio Rose"
+            />
+            <span className="models-tag">Novo</span>
+            <h4 className="models-title">Apple Watch SE</h4>
+            <p className="models-description">
+              A partir de <strong>R$ 3.399</strong>
+            </p>
+          </div>
+
+          <div className="models-item" id="model-3">
+            <img
+              src="https://lh3.googleusercontent.com/fife/AK0iWDyRKd4Je5-QAhAntv5Se51SVTERtCKTe_HTjCsWFpAOPcoZzmwGk89JJeSA3lKX5l989ZzSDgOCJ44ZLKoNB8YtFXlVMBslVw0Ex1YNWTHOilEWVkE7uTYkJkQRXdz1Af4jDYY5bXC-d8_pZKUkp5U4V7JPpW-348wtK42tQm4mX-KiLE8tUKtlexpDrNuNt0zre9s5UVD1wlAtieMgtdcDmASebya1jP7z0QLePq1kioAvuZkpwbEhI1_3wcpTPmDliYJjJaWpisQWgFcXgfjb5Br2jltz_Z6Af4tkg0yRzQe6dWGoEg-QRmdADSRpsChovXfk_jDami0-lTLD-BXcqWSj9slnbBPXRNOxSh6m3O6UIvOvSiFRaU-wDIjkRVI23gZ2EwP9hxYkqQdvLXZ4-IHwBNek_U9jXG9WmnZE-28JqBrK2k89zwbY3A1Xa6KiqfylgJiCBh2ngaQaLYNbCHcOorVNTofHvBAk1Ad3eBvlxrpxguscDttSy31AyTMabscHTfhljSoi2BE6Nr0biTrRqCaG1jYI08t5aPc3Y0K6u-Umsalw_9hN5lm6XV5pkhMO_eXV-0zA2rWnkOjieOiQSBoz2FA4rVx_qNbnGLDQ9AkjA_NMiHTl4LahcGvloiK36F4Zcoy9oKgH22Pi_Aq75cQoaTPEsteA_1u_iNVcKMz40wME6qlrepV_iAY1SNCmG08SkP044U3vCYUPqZp9IqgDOhdtacWe5AjTn-M4e0jEBVG85bM4TbAAYEFQFgeDrIP0GfKnxYcypmVzipsaqXXzMw4YT7W9h0wZKNzXJ2szV47qj5n67LrtyKbQUcSKuXQylbH8iWZ6kR3ShX6sI2wvS0Jfcash-yBXLwxq-mL1cRL3EbERGl-0tVB-_qDnnc2VOvurbbo8WiM7mZYkbxM9UGyJbMciW6NdYbYu0Bg-bvRRDWu2B4Qwr1GQuQJCrWaSpKGpmfRRs9gwvqft9nGAIFQLgjYjtvUWSFxGJ9JT8qnk3H27qzma-vFTrJCGSo1Y5G-AuGPmZYIhApk4mAPHAn8u5otXn-ayWAM45F4P5vUVn7hAA4epQ1Z7Q6g-voWzJFomAN1wefmMXbWajlJRkhiuq8VdgpvScVLpbyMCB5ZyBHqeu0NSH-FGekNHKJYLVovAXaiMrrlt3YOA2UZ7YjpL4Mvx9cISh0jPZOV7t9yQT6JjrBj672q-tt5nNDWJsXHtj9S8_8rdURlJTTV2fmwycTGSmOdgBYcoZOMQtO-gVwnlYEUOVNQm1QV55A9ZRCG-L7X_MHvVhjtGHyFmK073iMGCgsxVuacGwWPoyeyjh2JTCXFdeZWmMA7SqOQGrhBP-Pv_2uqg-N24H0zxBHC28wLYtFNcN8iF9EDJTnePJBmyXxBGodBj-oC5dUQAJwj1is_KqoKL2ozeQ_meEdl9zU3udKoklx4e9Dcjqt2MW1lDxtt6uW4aHmmgs3rBfF61VAJNVC-Si6K17wsan9kWfD-zJArNx9kSchtddW78K-3tchYoOqjgMhI8hSw=w2880-h1508"
+              alt="Relogio Ultra"
+            />
+            <span className="models-tag">Novo</span>
+            <h4 className="models-title">Apple Watch Ultra</h4>
+            <p className="models-description">
+              A partir de <strong>R$ 10.299</strong>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="area-model">
+        <h1>ITEM 3</h1>
+      </div>
+
+      <div className="area-model">
+        <h1>ITEM 4</h1>
+      </div>
     </div>
   );
 }
